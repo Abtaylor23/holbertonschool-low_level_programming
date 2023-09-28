4af21e05b339d91f91bcb008055f181e74641cca
Abtaylor23 <vaultmoney420@gmail.com>
2023-09-28 12:09:39 -0700

    added 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,24 @@
  *    */
 void print_alphabet_x10(void)
 {
-	char ch = 'a';
-	int i = 0;
-	
-	while (i < 10)
-	{
-		while (ch <= 'z')
+	char ch = 'anclude "main.h"
+		
+		/**
+		 *  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+		 *   * followed by a new line
+		 *    */
+		void print_alphabet_x10(void)
 		{
-			_putchar(ch);
-			ch++;
-		}
-	_putchar('\n');
-	i++;
-	ch = 'a';
+			char ch;
+			int i;
+			i = 0;
+			while (i < 10)
+				ch = 'a';
+			while (ch <= 'z')								{
+				_putchar(ch);
+				ch++;
+			}
+				_putchar('\n');
+				i++;
+}
 }
