5d1b4ae22f12b278f0395ae1fdb7380282014110
Abtaylor23 <vaultmoney420@gmail.com>
2023-09-28 11:56:33 -0700

    added 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,17 @@
  *    */
 void print_alphabet_x10(void)
 {
-	char ch;
+	char ch = 'a';
 	int i = 0;
+	
+	while (i < 10)
 	{
-		while (i < 10)
-			ch = 'a';
 		while (ch <= 'z')
-	{
-		_putchar(ch);
+		{
+			_putchar(ch);
 			ch++;
-	}
+		}
 	_putchar('\n');
 	i++;
-	}
+	ch = 'a';
 }
