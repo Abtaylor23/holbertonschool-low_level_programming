9022d712e090b3485f20a085c0ee0daae24fec3e
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-01 14:45:38 -0700

    added 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - int the vaule n 
+ * and update the vaule it points to 98
+ * @n: int n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+collection of C - Pointers, arrays and strings
