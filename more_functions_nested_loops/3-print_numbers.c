178f497a493a287fb78b7f6832ac90fb53536f8a
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-02 08:10:00 -0700

    added 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - Print the numbers since 0 up to 9
  * Return: The numbers since 0 up to 9
@@ -13,7 +12,7 @@ void print_numbers(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-	putchar(x = '0' );
+	putchar(x + '0' );
 	}
 	putchar('\n');
 }
