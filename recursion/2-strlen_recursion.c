4169facf589e53d85d4b21b73db790f527178b5c
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-11 14:29:43 -0700

    added 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strien_recursion -Returns the length of a string
- * @s; The string to be measured
+ * _strien_recursion - Returns the length of a string
+ * @s: The string to be measured
  *
  * Return: The length of the string
  */
