60484959c8ae05e7fa0eee85e6345cb176bd6a5c
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-13 11:55:02 -0700

    added 4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * _pow_recursion - Return the vaule of x raised to the power of y
- * @x: the value to multiply    
+ * @x: the value to multiply  
  * @y: the times to multiply the vaule
  * Return: the vaule multiplied y times
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1);                                                                             
+		return (-1);
+	
 	if (y == 0)
 		return (1);
 	
