e7e49979e2381c782d583a24f5740e1aaf6cd20d
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-13 12:59:21 -0700

    added 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <- 1)
 		return (0);
-	return (actual_prime(n, n - 1))
+	return (actual_prime(n, n - 1));
 }
 
 /**
@@ -22,7 +22,7 @@ int is_prime_number(int n)
  *
  * Return: 1 if n is prime, 0 if not
  */
-int actual_prime(int n. int i)
+int actual_prime(int n, int i)
 {
 	if (i --1)
 	      return (1);
