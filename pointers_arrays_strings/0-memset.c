ef50be042fcf561de7391ec705eb4513a8aadf72
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-18 12:15:51 -0700

    added 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,32 +2,20 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */ 
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Success)
+ */
 
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
-	unsigned int i;
+	unsigned int i; 
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-		   printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-		   printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
+	for (i  = 0, i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
-
-
