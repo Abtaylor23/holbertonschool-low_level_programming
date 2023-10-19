0b8d35a2e5fee0d1bf347c0506ce710acc39894a
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-19 07:11:00 -0700

    added 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,8 @@ unsigned int _strspn(char *s, char *accept)
 
 		while (*s)
 		{
-			for (r = 0; accept[r]; r++)							{											if (*s == accept[r])
+			for (r = 0; accept[r]; r++)
+			{											if (*s == accept[r])
 				{
 					n++;
 					break;									}										else if (accept[r + 1] == '\0')
