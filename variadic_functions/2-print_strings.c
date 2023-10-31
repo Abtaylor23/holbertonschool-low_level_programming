fc4b567fca923d51da6bcead69a8196c9a05eadc
Abtaylor23 <vaultmoney420@gmail.com>
2023-10-30 20:34:50 -0700

    added 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
+#include <stddef.h>
 /**
  * print_strings - print strings passed to a function
  * as paramerter 
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	
 	va_start(ptr_str, n);
 
-	for (i = 0, i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ptr_str, char *);
 
