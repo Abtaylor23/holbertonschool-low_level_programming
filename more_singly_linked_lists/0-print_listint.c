ebf4dc424713ac5cb9ddda63f69ff8317b9ced2c
Abtaylor23 <vaultmoney420@gmail.com>
2023-11-13 20:33:09 -0800

    added 0-print_listint.c

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  *
  * Return: the number of nodes printed
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const list_t *h)
 {
 size_t num = 0;
 
 while (h)
 {
-printf("%d\n", h->n);
+printf("%d\n", h->h);
 num++;
 h = h->next;
 }
