ecadeb5dd0a237c32ad5d4a9f9c313b42be2b2be
Abtaylor23 <vaultmoney420@gmail.com>
2023-11-13 20:20:09 -0800

    added 4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,16 +1,21 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_list - Frees a singly linked list.
+ * @head: A pointer to the head of the list.
+ *
+ * Return: void.
  */
-void free_list_t(list_t *head)
+void free_list(list_t *head)
 {
-list_t *temp;
-while (head)
-{
-temp = head->next;
-free(head);
-head = temp;
-}
+	list_t *tmp_node; /* Create a temporary node */
+
+	while (head)
+	{
+		tmp_node = head;
+		head = head->next;
+		free(tmp_node->str);
+		free(tmp_node);
+	}
 }
