d5200dfcaa83c30b4a782d092b66cef419bd4b8c
Abtaylor23 <vaultmoney420@gmail.com>
2023-11-20 18:34:28 -0800

    added 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,8 +10,8 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint *new;
-	dlistint *h;
+	dlistint_t *new;
+	dlistint_t *h;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
