85b86d598e10417627d124685388b894de237f1f
Abtaylor23 <vaultmoney420@gmail.com>
2023-11-20 20:00:28 -0800

    added 3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,12 +11,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *tmp;
 
 	if ((*head != NULL))
-		while ((*head)->prev != NULL)
+	{	while ((*head)->prev != NULL)
 			*head = (*head)->prev;
-
+	}
 	while ((tmp = *head) != NULL)
-
+	{
 		*head = (*head)->next;
 		free(tmp);
-
+	}
 }
